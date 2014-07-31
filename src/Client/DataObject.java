6bf2a594e7b84ba2104f1e2ce427bfb72d1f8053
m4th3r <mathieulavallee@gmail.com>
2014-07-31 11:22:09 -0400

    xstream gone

--- src/Client/DataObject.java
+++ src/Client/DataObject.java
@@ -5,7 +5,6 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-import com.thoughtworks.xstream.XStream;
 
 
 @XmlRootElement(name = "file")
